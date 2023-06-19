76aed00a148c772fc4ea3430605fedb6a6812b8f
Thiago S. Brasil <thiagosbrasil@ua.pt>
2023-06-19 18:30:52 +0100

    feat: :sparkles: socketio conn

--- visor-api/index.js
+++ visor-api/index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
 const mongoString = process.env.MONGODB_URL;
 const routes = require('./routes/routes');
 
@@ -20,10 +23,28 @@ db.once('connected', () => {
 })
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('Socket Connected: ' + socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Socket Disconnected: " + socket.id);
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
--- visor-ui/src/app/page.js
+++ visor-ui/src/app/page.js
@@ -1,10 +1,22 @@
-import Image from 'next/image'
+'use client'
+
+import io from 'socket.io-client'
+import { useState, useEffect } from 'react'
+
+const socket = io('http://localhost:3005')
 
 export default function Home() {
+
+  const [socketInfo, setSocketInfo] = useState(null)
+
+  useEffect(() => {
+    setSocketInfo(socket)
+  }, [socket])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-       
+        Hello, {socketInfo && socketInfo.id}
       </div>
     </main>
   )
