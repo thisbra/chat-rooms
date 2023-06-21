161ac5297a82b2ba60054e2ff40622eafa19e37e
Thiago S. Brasil <thiagosbrasil@ua.pt>
2023-06-21 15:30:24 +0100

    feat: :sparkles: restfull for all entities

--- visor-api/src/routes/routes.js
+++ visor-api/src/routes/routes.js
@@ -2,57 +2,147 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../modules/user/user.model');
+const Room = require('../modules/room/room.model');
+const Message = require('../modules/message/message.model');
 
+// POST Method
+router.post('/users', async (req, res) => {
+  const user = new User({
+    userId: req.body.userId,
+    username: req.body.username
+  });
 
-//Post Method
-router.post('/post', async (req, res) => {
-    const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        companies: req.body.companies
-    })
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
-    try {
-        const newUser = await user.save();
-        res.status(200).json(newUser);
-    } catch (err) {
-        res.status(400).json({ message: err.message })
-    }
-})
-
-
-//Get all Method
+// GET All Method
 router.get('/users', async (req, res) => {
-    try{
-        const data = await User.find();
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-//Get by ID Method
-router.get('/user/:id', async (req, res) => {
+// GET by ID Method
+router.get('/users/:userId', async (req, res) => {
     try {
-        const data = await User.findById(req.params.id);
-        res.json(data)
+        const user = await User.findOne({ userId: req.params.userId });
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-    catch (error) {
-        res.status(500).json({ message: error.message })
+});
+
+// DELETE by ID Method
+router.delete('/users/:userId', async (req, res) => {
+    try {
+        await User.deleteOne({ userId: req.params.userId });
+        res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-})
+});
 
-//Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
-})
 
-//Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
-})
+// POST Method for rooms
+router.post('/rooms', async (req, res) => {
+    try {
+      const room = new Room({
+        id: req.body.roomId,
+        name: req.body.name,
+        messageList: []
+      });
+      const newRoom = await room.save();
+      res.status(201).json(newRoom);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+});
+
+// GET All Method for rooms
+router.get('/rooms', async (req, res) => {
+try {
+    const rooms = await Room.find();
+    res.json(rooms);
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
+});
+
+// GET by ID Method for rooms
+router.get('/rooms/:roomId', async (req, res) => {
+try {
+    const room = await Room.findOne({ id: req.params.roomId });
+    res.json(room);
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
+});
+
+// DELETE by ID Method for rooms
+router.delete('/rooms/:roomId', async (req, res) => {
+try {
+    await Room.deleteOne({ id: req.params.roomId });
+    res.json({ message: 'Room deleted successfully' });
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
+});
+
+// POST Method for messages
+router.post('/messages', async (req, res) => {
+try {
+    const message = new Message({
+    id: req.body.messageId,
+    content: req.body.content,
+    timestamp: req.body.timestamp,
+    author: req.body.author,
+    room: req.body.room
+    });
+    const newMessage = await message.save();
+    res.status(201).json(newMessage);
+} catch (err) {
+    res.status(400).json({ message: err.message });
+}
+});
+
+// GET All Method for messages
+router.get('/messages', async (req, res) => {
+try {
+    const messages = await Message.find();
+    res.json(messages);
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
+});
+
+// GET by ID Method for messages
+router.get('/messages/:messageId', async (req, res) => {
+try {
+    const message = await Message.findOne({ id: req.params.messageId });
+    res.json(message);
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
+});
+
+// DELETE by ID Method for messages
+router.delete('/messages/:messageId', async (req, res) => {
+try {
+    await Message.deleteOne({ id: req.params.messageId });
+    res.json({ message: 'Message deleted successfully' });
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
+});
+
 
 
 module.exports = router;
