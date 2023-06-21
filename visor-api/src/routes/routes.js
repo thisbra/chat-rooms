60e3aff3dbe51e4a95982d0d5bae62c78e2ff8af
Thiago S. Brasil <thiagosbrasil@ua.pt>
2023-06-21 14:49:00 +0100

    refactor: :recycle: backend restructuring

--- visor-api/src/routes/routes.js
+++ visor-api/src/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/user');
+const User = require('../modules/user/user.model');
 
 
 //Post Method
