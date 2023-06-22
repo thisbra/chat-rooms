8ab49ba92b8e7ec324965feeef16944066689b94
Thiago S. Brasil <thiagosbrasil@ua.pt>
2023-06-22 15:53:59 +0100

    feat: :globe_with_meridians: home redirects to chat and minor style fix

--- visor-ui/src/app/page.js
+++ visor-ui/src/app/page.js
@@ -1,23 +1,18 @@
 'use client'
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-import io from 'socket.io-client'
-import { useState, useEffect } from 'react'
+export default function Page() {
 
-const socket = io('http://localhost:3005')
-
-export default function Home() {
-
-  const [socketInfo, setSocketInfo] = useState(null)
-
+  const router = useRouter();
+  
   useEffect(() => {
-    setSocketInfo(socket)
-  }, [socket])
+    router.push('/chat');
+  }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Hello, {socketInfo && socketInfo.id}
-      </div>
+    <main className="page-container">
+      
     </main>
   )
 }
